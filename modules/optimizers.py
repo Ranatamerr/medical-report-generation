f462e921a2e9499058ae5784522b8669ea9921ae
ranatamerr <roneytamer@gmail.com>
2026-04-09 01:23:04 +0200

    Remove verbose arg from ReduceLROnPlateau for newer PyTorch compatibility

--- modules/optimizers.py
+++ modules/optimizers.py
@@ -100,7 +100,7 @@ class ReduceLROnPlateau(object):
     def __init__(self, optimizer, mode='min', factor=0.1, patience=10, verbose=False, threshold=0.0001,
                  threshold_mode='rel', cooldown=0, min_lr=0, eps=1e-08):
         self.scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer=optimizer, mode=mode, factor=factor,
-                                                              patience=patience, verbose=verbose, threshold=threshold,
+                                                              patience=patience, threshold=threshold,
                                                               threshold_mode=threshold_mode, cooldown=cooldown,
                                                               min_lr=min_lr, eps=eps)
         self.optimizer = optimizer
